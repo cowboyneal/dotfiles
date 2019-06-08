406b635d3e6cc4655521c1cfe71295d5733b820b
Jonathan Pater <cowboyneal@gmail.com>
2019-06-08 17:52:44 -0400

    Let metar take an arg

--- .config/polybar/metar.sh
+++ .config/polybar/metar.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-temp_c=$(curl -s https://w1.weather.gov/data/METAR/KOSU.1.txt | grep METAR | awk '{ print $7 }' | sed -r 's/^([0-9]*)\/[0-9]*$/\1/')
-
+temp_c=$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt | grep METAR | awk '{ print $7 }' | sed -r 's/^([0-9]*)\/[0-9]*$/\1/')
 echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
