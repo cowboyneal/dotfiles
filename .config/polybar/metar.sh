b3221029f8740282416ca5bf2a2835984dcbf098
Jonathan Pater <cowboyneal@gmail.com>
2019-06-08 12:56:25 -0400

    Add metar applet

---
+++ .config/polybar/metar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+temp_c=$(curl -s https://w1.weather.gov/data/METAR/KOSU.1.txt | grep METAR | awk '{ print $7 }' | sed -r 's/^([0-9]*)\/[0-9]*$/\1/')
+
+echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
