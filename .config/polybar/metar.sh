9cfda669150defb40313798d29430e7cab9955fa
Jonathan Pater <cowboyneal@gmail.com>
2019-06-08 23:42:07 -0400

    Fix regex to work in bsd

--- .config/polybar/metar.sh
+++ .config/polybar/metar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-temp_c=$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt | grep METAR | sed -r 's/^.*\s([0-9]*)\/[0-9]*.*/\1/')
+temp_c=$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt | grep METAR | sed -r 's/^.*[[:space:]]([0-9]*)\/[0-9]*.*/\1/')
 echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
