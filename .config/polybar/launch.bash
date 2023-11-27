fb395d92c15f02a032fe71b8d3bdfafef1308207
Jonathan Pater <cowboyneal@gmail.com>
2023-11-27 11:16:10 -0500

    Shuffle bars around a bit

--- .config/polybar/launch.bash
+++ .config/polybar/launch.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+pkill -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar topbar &
+polybar bottombar &
 
 echo "Bars launched..."
