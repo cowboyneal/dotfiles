42d9498cfc64d83f5b6770e289e9922163610fcf
Jonathan Pater <cowboyneal@gmail.com>
2024-05-08 13:57:09 -0400

    Remove a line

--- .config/polybar/metar.sh
+++ .config/polybar/metar.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-temp_c="$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt \
-    | tr '\r\n' ' ')\n"
-temp_c=$(echo "$temp_c" | awk 'BEGIN { FS = "/" } ; { print $1 }' \
+temp_c=$(echo "$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt \
+    | tr '\r\n' ' ')\n" | awk 'BEGIN { FS = "/" } ; { print $1 }' \
     | sed -r 's/^.*[[:space:]](M?[0-9]*)$/\1/' | sed 's/M/-/')
 echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l \
     | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
