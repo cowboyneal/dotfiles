0e1e69832b751f013d8a8a144c6027bc1bdb0e70
Jonathan Pater <cowboyneal@gmail.com>
2024-05-08 08:08:13 -0400

    Fix metar.sh for NWS advisory conditions

--- .config/polybar/metar.sh
+++ .config/polybar/metar.sh
@@ -1,4 +1,8 @@
-#!/bin/ksh
+#!/usr/bin/env bash
 
-temp_c=$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt | grep METAR | sed -r 's/^.*[[:space:]](M?[0-9]*)\/M?[0-9]*.*/\1/' | sed 's/M/-/')
-echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
+temp_c="$(curl -s https://w1.weather.gov/data/METAR/${1}.1.txt \
+    | tr '\r\n' ' ')\n"
+temp_c=$(echo "$temp_c" | awk 'BEGIN { FS = "/" } ; { print $1 }' \
+    | sed -r 's/^.*[[:space:]](M?[0-9]*)$/\1/' | sed 's/M/-/')
+echo "($temp_c * 9.0 / 5.0) + 32.0" | bc -l \
+    | sed -r 's/^([0-9]*\.[0-9])[0-9]*$/\1/'
