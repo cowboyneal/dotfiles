f929663a897458137acd2f564b06ab9f5aa49180
Jonathan Pater <cowboyneal@gmail.com>
2026-01-23 15:17:02 -0500

    Add sudo script

--- sudo_config.bash
+++ sudo_config.bash
@@ -1,10 +1,17 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+
+name="$1"
+
+if [ -z "$name" ]; then
+    echo "need a user ID"
+    exit 1
+fi
 
 dir_locs=( '/usr/local/etc/sudoers.d' '/etc/sudoers.d' )
 
 for a in ${dir_locs[@]}; do
     if [ -d "$a" ]; then
-        echo "pater ALL=(ALL:ALL) NOPASSWD" > $a/pater
+        echo "$name ALL=(ALL:ALL) NOPASSWD: ALL" > $a/sudo-$name
         break
     fi
 done
